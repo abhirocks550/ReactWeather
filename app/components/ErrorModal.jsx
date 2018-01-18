c86aecf03b6c7bba13f285cadafaad226a5d6c24
abhirocks550 <abhirocks550@gmail.com>
2018-01-18 18:09:40 +0530

    Fix error modal bug

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -17,12 +17,14 @@ var ErrorModal = React.createClass({
   render() {
     let { title, message } = this.props;
     return (
-      <div id="error-modal" className="reveal tiny text-center" data-reveal="">
-        <h4>{title}</h4>
-        <p>{message}</p>
-        <p>
-          <button className="button hollow" data-close="">Okay</button>
-        </p>
+      <div>
+        <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+          <h4>{title}</h4>
+          <p>{message}</p>
+          <p>
+            <button className="button hollow" data-close="">Okay</button>
+          </p>
+        </div>
       </div>
     );
   }
